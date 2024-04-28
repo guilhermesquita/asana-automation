ea972ac1960fdde46d9ea558edb3d52b89961954
guilhermesquita <guilhermer_mesquita@hotmail.com>
2024-04-27 23:40:49 -0400

    feat: convert data to spreadsheet

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-# import pandas as pd
-# from IPython.display import display
-# from openpyxl import load_workbook
-
-# tabela = pd.read_excel("Migracao.xlsx", sheet_name="Processos  - Preenchimento", header=1)
-# display(tabela)
-# tabela_data=[]
-# for coluna in tabela:
-#     tabela_data.append({
-#         'Título': coluna['nome']
-#     })
-
 import requests
+import openpyxl
 
 offset = ''
 data_task = []
 
-url_base = 'https://app.asana.com/api/1.0/sections/1201422389995890/tasks?opt_fields=&limit=1&{offset}'.format(
+url_base = 'https://app.asana.com/api/1.0/sections/1201422389995890/tasks?opt_fields=&limit=3&{offset}'.format(
     offset=offset)
 headers = {
     'Authorization': 'Bearer 2/1202126083545971/1207182145066539:9de56ef74e356640cc82f4a05c7fd7b7',
@@ -31,7 +20,6 @@ def get_task_data(url, headers):
         print('Falha na requisição. Código de status:', response.status_code)
         return None
 
-
 while True:
     data = get_task_data(url_base, headers)
     if not data:
@@ -45,12 +33,17 @@ while True:
             task_data = get_task_data(url_task, headers)
             if not task_data:
                 continue
-
             data_json = task_data.get('data')
+
+            indice_inicio_telefone = data_json['notes'].find('Telefone:')
+
+            if indice_inicio_telefone != -1:
+                indice_fim_telefone = data_json['notes'].find('\n', indice_inicio_telefone)                
+                if indice_fim_telefone != -1:
+                    number_telephone = data_json['notes'][indice_inicio_telefone+len('Telefone:'):indice_fim_telefone].strip()
+
             if not data_json:
                 continue
-
-            print(data_json['name'])
 
             url_story = "https://app.asana.com/api/1.0/tasks/{gid}/stories".format(gid=gid)
             story_data = get_task_data(url_story, headers)
@@ -61,24 +54,24 @@ while True:
 
             for story in data_story:
                 if story['type'] == 'comment' and 'Processo: ' in story['text']:
-                    processonumero = story['text'].split('Processo: ')[1].split()[0]
+                    processnumber = story['text'].split('Processo: ')[1].split()[0]
                     break
             else:
-                processonumero = None
+                processnumber = None
 
-            envolved = objective = None
+            involved = objective = None
             if 'tags' in data_json and len(data_json['tags']) > 2 and data_json['tags'][2] is not None and 'name' in data_json['tags'][2]:
-                envolved = data_json['tags'][2]['name']
+                involved = data_json['tags'][2]['name']
             if 'tags' in data_json and len(data_json['tags']) > 2 and data_json['tags'][1] is not None and 'name' in data_json['tags'][1]:
                 objective = data_json['tags'][1]['name'] 
 
-            data_task.append({'name': data_json['name'], 'numero': processonumero, 'envolvido': envolved,
-                              'objeto': objective})
+            data_task.append({'name': data_json['name'], 'number': processnumber, 'involved': involved,
+                              'objeto': objective,'telephone': number_telephone})
             print(data_task)
 
         if data.get('next_page'):
             offset = 'offset=' + data['next_page']['offset']
-            url_base = 'https://app.asana.com/api/1.0/sections/1201422389995890/tasks?opt_fields=&limit=1&{offset}'.format(
+            url_base = 'https://app.asana.com/api/1.0/sections/1201422389995890/tasks?opt_fields=&limit=100&{offset}'.format(
                 offset=offset)
         else:
             break
@@ -87,6 +80,33 @@ while True:
         print('Não há mais tarefas.')
         break
 
-# print(data_task)
-# else:
-#     print('Falha na requisição. Código de status:', response.status_code)
+print(data_task)
+def preencher_planilha(data_task, planilha_excel):
+    wb = openpyxl.load_workbook(planilha_excel)
+    ws = wb['Processos  - Preenchimento']
+    ws2 = wb['Contatos  - Preenchimento']
+
+    for row, task_data in enumerate(data_task, start=3):  
+        ws.cell(row=row, column=2, value=task_data.get('name', ''))  
+        ws.cell(row=row, column=5, value=task_data.get('number', ''))  
+        ws.cell(row=row, column=6, value=0)
+        ws.cell(row=row, column=7, value='indenizatória')
+        ws.cell(row=row, column=8, value=1)
+        ws.cell(row=row, column=15, value=task_data.get('name', ''))  
+        ws.cell(row=row, column=17, value='Reclamante')  
+        ws.cell(row=row, column=18, value=task_data.get('involved', ''))  
+        ws.cell(row=row, column=20, value=task_data.get('objective', ''))  
+        ws.cell(row=row, column=23, value=task_data.get('objective', ''))  
+        ws.cell(row=row, column=30, value='contato@barbosaluan.com')   
+
+    for row, task_data in enumerate(data_task, start=2):  
+        ws2.cell(row=row, column=1, value=task_data.get('name', ''))  
+        ws2.cell(row=row, column=5, value=task_data.get('telephone', ''))    
+
+
+    wb.save(planilha_excel)
+
+planilha_existente = 'Migracao.xlsx'
+teste = [{"name": 'Wesley', "age": 13}, {"name": 'Luiz', "age": 20}, {"name": 'Gui', "age": 19}]
+preencher_planilha(data_task, planilha_existente)
+print("Informações já exportadas")
